4b14a25b190e24ed92c164de77048dbb61a1335d
Lachlan Donald <lachlan@ljd.cc>
2024-12-08 14:03:11 +1100

    chore: Release please config for v1.0.0

--- cmd/gotestchunk/main.go
+++ cmd/gotestchunk/main.go
@@ -10,8 +10,12 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// Version is the current version of gotestchunk
+const Version = "1.0.0" // x-release-please-version
+
 var cli struct {
-	Debug bool `short:"d" help:"Enable debug logging"`
+	Debug   bool `short:"d" help:"Enable debug logging"`
+	Version bool `short:"V" help:"Show version information"`
 
 	List commands.ListCmd `cmd:"" help:"List tests in packages"`
 	Test commands.TestCmd `cmd:"" help:"Run tests for a specific chunk" default:"withargs"`
@@ -32,7 +36,16 @@ func main() {
 		kong.Name("gotestchunk"),
 		kong.Description("A tool for listing and chunking Go tests"),
 		kong.UsageOnError(),
+		kong.Vars{
+			"version": Version,
+		},
 	)
+
+	if cli.Version {
+		fmt.Printf("gotestchunk version %s\n", Version)
+		os.Exit(0)
+	}
+
 	err := ctx.Run(&logger)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
